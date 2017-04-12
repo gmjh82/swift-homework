db2c3010206c50b7490c850509660ace9bd8efca
윤장희 <yunjanghee@yunjanghuiui-MacBook-Pro.local>
2017-04-13 00:06:26 +0900

    data type stror Int

---
+++ 02_data_types.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// implicit
+var str = "Hello, playground"
+//str = 3
+// var & let : 한번 넣은 파일을 변경할 수 있으면 var, 없으면 let
+
+var number = 1
+
+//explict
+
+//String 문자열
+var str2:String = "hello"
+//var str3:string = 1
+
+//Int 정수
+var number2:Int = 2
+//var number2:Int = "playground"
+
+// convert string to integer
+10 + 11
+"10" + "11"
+//"10" + 11
+
+// 문자를 숫자로
+var numStr = "10"
+//var numStr = "hello"
+Int(numStr)
+
+// convert integer to stiring
+var number3 = 5
+var str3 = "hello"
+"\(number3)"
+"\(str3)! \(number3)"
+String(number3)
+
+// integer
+3 / 2
+
+// doublel / float
+3.0 / 2.0
+
+// double to inteager
+Int(3.0 / 2.0)
+
+
+
